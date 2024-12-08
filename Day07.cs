07f0957dba176819679239eaab28aeb280226394
Joey Parker <ninjajkl@gmail.com>
2024-12-08 00:11:58 -0500

    feat: added day 7

---
+++ Day07.cs
@@ -0,0 +1,70 @@
+﻿namespace AdventOfCode2024
+{
+    internal class Day07
+    {
+        public string Part1(string inputName)
+        {
+            string[] RawInput = System.IO.File.ReadAllLines(inputName);
+
+            long sum = 0;
+            foreach (string line in RawInput)
+            {
+                List<long> values = line.Replace(":", "").Split(' ').Select(long.Parse).ToList();
+                long targetNum = values[0];
+                values.RemoveAt(0);
+                long nextVal = values[0];
+                values.RemoveAt(0);
+                sum += checkNextOp(nextVal, values, targetNum) ? targetNum : 0;
+            }
+            return sum.ToString();
+        }
+
+        public string Part2(string inputName)
+        {
+            string[] RawInput = System.IO.File.ReadAllLines(inputName);
+
+            long sum = 0;
+            foreach (string line in RawInput)
+            {
+                List<long> values = line.Replace(":", "").Split(' ').Select(long.Parse).ToList();
+                long targetNum = values[0];
+                values.RemoveAt(0);
+                long nextVal = values[0];
+                values.RemoveAt(0);
+                sum += checkNextOpPlus(nextVal, values, targetNum) ? targetNum : 0;
+            }
+            return sum.ToString();
+        }
+
+        public bool checkNextOp(long currVal, List<long> remainingValues, long targetVal)
+        {
+            if (remainingValues.Count == 0)
+            {
+                return currVal == targetVal;
+            }
+
+            long plus = currVal + remainingValues[0];
+            long mult = currVal * remainingValues[0];
+
+            List<long> nextValues = new(remainingValues);
+            nextValues.RemoveAt(0);
+            return checkNextOp(plus, nextValues, targetVal) || checkNextOp(mult, nextValues, targetVal);
+        }
+
+        public bool checkNextOpPlus(long currVal, List<long> remainingValues, long targetVal)
+        {
+            if (remainingValues.Count == 0)
+            {
+                return currVal == targetVal;
+            }
+
+            long plus = currVal + remainingValues[0];
+            long mult = currVal * remainingValues[0];
+            long concat = long.Parse(currVal.ToString() + remainingValues[0].ToString());
+
+            List<long> nextValues = new(remainingValues);
+            nextValues.RemoveAt(0);
+            return checkNextOpPlus(plus, nextValues, targetVal) || checkNextOpPlus(mult, nextValues, targetVal) || checkNextOpPlus(concat, nextValues, targetVal);
+        }
+    }
+}
--- obj/Debug/net8.0/AdventOfCode2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdventOfCode2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e48f1181502c63b7479c7bf3249533c79ee62b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df6d025eb3694e95906a3f5aaa5ddd7a65d178e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
