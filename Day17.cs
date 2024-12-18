66fa91f98dc77994053cac442ba9014ed0f74a17
Joey Parker <Joseph.Parker@Charter.com>
2024-12-18 15:58:52 -0500

    feat; added partial day 17

--- Day17.cs
+++ Day17.cs
@@ -10,9 +10,9 @@ namespace AdventOfCode2024
         {
             string[] RawInput = File.ReadAllLines(inputName);
 
-            int A = int.Parse(Regex.Matches(RawInput[0], pattern)[0].Value);
-            int B = int.Parse(Regex.Matches(RawInput[1], pattern)[0].Value);
-            int C = int.Parse(Regex.Matches(RawInput[2], pattern)[0].Value);
+            long A = long.Parse(Regex.Matches(RawInput[0], pattern)[0].Value);
+            long B = long.Parse(Regex.Matches(RawInput[1], pattern)[0].Value);
+            long C = long.Parse(Regex.Matches(RawInput[2], pattern)[0].Value);
 
             int[] instructions = Regex.Matches(RawInput[4], pattern).Select(x => int.Parse(x.Value)).ToArray();
             int instructionPointer = 0;
@@ -44,71 +44,16 @@ namespace AdventOfCode2024
                         instructionPointer += 2;
                         break;
                     case 5: //out
-                        int val = CO(operand, A, B, C) % 8;
+                        long val = CO(operand, A, B, C) % 8;
                         output += output.Length == 0 ? val : "," + val;
                         instructionPointer += 2;
                         break;
                     case 6: //bdv
-                        B = A / (int)Math.Pow(2, CO(operand, A, B, C));
+                        B = A / (long)Math.Pow(2, CO(operand, A, B, C));
                         instructionPointer += 2;
                         break;
                     case 7: //cdv
-                        C = A / (int)Math.Pow(2, CO(operand, A, B, C));
-                        instructionPointer += 2;
-                        break;
-                }
-            }
-            return output;
-        }
-
-        public string Part1Old(string inputName)
-        {
-            string[] RawInput = File.ReadAllLines(inputName);
-
-            int A = int.Parse(Regex.Matches(RawInput[0], pattern)[0].Value);
-            int B = int.Parse(Regex.Matches(RawInput[1], pattern)[0].Value);
-            int C = int.Parse(Regex.Matches(RawInput[2], pattern)[0].Value);
-
-            int[] instructions = Regex.Matches(RawInput[4], pattern).Select(x => int.Parse(x.Value)).ToArray();
-            int instructionPointer = 0;
-            string output = "";
-
-            while (instructionPointer < instructions.Length)
-            {
-                int instruction = instructions[instructionPointer];
-                int operand = instructions[instructionPointer + 1];
-                switch (instruction)
-                {
-                    case 0: //adv
-                        A /= (int)Math.Pow(2, CO(operand, A, B, C));
-                        instructionPointer += 2;
-                        break;
-                    case 1: //bxl
-                        B ^= operand;
-                        instructionPointer += 2;
-                        break;
-                    case 2: //bst
-                        B = CO(operand, A, B, C) % 8;
-                        instructionPointer += 2;
-                        break;
-                    case 3: //jnz
-                        instructionPointer = A == 0 ? instructionPointer + 2 : operand;
-                        break;
-                    case 4: //bxc
-                        B ^= C;
-                        instructionPointer += 2;
-                        break;
-                    case 5: //out
-                        int val = CO(operand, A, B, C) % 8;
-                        output += output.Length == 0 ? val : "," + val;
-                        instructionPointer += 2;
-                        break;
-                    case 6: //bdv
-                        B = A / (int)Math.Pow(2, CO(operand, A, B, C));
-                        instructionPointer += 2;
-                        break;
-                    case 7: //cdv
-                        C = A / (int)Math.Pow(2, CO(operand, A, B, C));
+                        C = A / (long)Math.Pow(2, CO(operand, A, B, C));
                         instructionPointer += 2;
                         break;
                 }
@@ -120,14 +65,167 @@ namespace AdventOfCode2024
         {
             string[] RawInput = File.ReadAllLines(inputName);
 
-            int value = 100;
-            ref int v2 = ref value;
-            v2 = 200;
+            UnknownNumber A = new();
+            A.SetToSingle(0);
+            UnknownNumber B = new();
+            UnknownNumber C = new();
 
-            return value.ToString();
+            string stringOut = RawInput[4][9..];
+            int[] program = stringOut
+                .Split(',')
+                .Select(int.Parse)
+                .Reverse()
+                .ToArray();
+
+            (int instruction, int operand)[] instructions = program
+                .Reverse()
+                .Select((value, index) => new { value, index })
+                .GroupBy(x => x.index / 2)
+                .Select(g => (g.ElementAt(0).value, g.ElementAt(1).value))
+                .Reverse()
+                .ToArray();
+            int instructionPointer = 0;
+            int programIndex = 0;
+
+            while (programIndex != program.Length || instructionPointer != 0)
+            {
+                (int instruction, int operand) = instructions[instructionPointer];
+
+                long divisor = -1;
+                switch (instruction)
+                {
+                    case 0: //adv
+                        if (operand is >= 0 and <= 3)
+                        {
+                            divisor = operand;
+                        }
+                        divisor = (int)Math.Pow(2, divisor);
+
+                        if (A.IsSingleNumber)
+                        {
+                            A.SetToRange(A.SingleVal * divisor, (A.SingleVal * divisor) + divisor - 1);
+                        }
+                        else
+                        {
+                            throw new NotImplementedException();
+                        }
+                        break;
+                    case 1: //bxl
+                        B.SetToSingle(B.SingleVal ^ operand);
+                        break;
+                    case 2: //bst
+                        ref UnknownNumber op = ref CO(operand, ref A, ref B, ref C);
+                        if (op.IsRange)
+                        {
+                            long result = op.Min + B.SingleVal - (op.Min % 8);
+                            if (result > op.Max)
+                            {
+                                throw new ArgumentOutOfRangeException(nameof(operand), "Invalid Value");
+                            }
+                            op.SetToSingle(result);
+                            break;
+                        }
+                        break;
+                    case 3: //jnz
+                        if (A.IsRange && A.Min == 0)
+                        {
+                            A.Min = 1;
+                        }
+                        break;
+                    case 4: //bxc
+                        long roundInitB = programIndex == program.Length ? 0 : program[programIndex];
+                        C.SetToSingle(B.SingleVal^5^1^roundInitB);
+                        B.SetToSingle(B.SingleVal ^ C.SingleVal);
+                        break;
+                    case 5: //out
+                        //Ignoring the cases where operand is 1,2,3 because that won't happen in a program bc it would just repeat the same output
+                        ref UnknownNumber un = ref CO(operand, ref A, ref B, ref C);
+                        int goalNum = program[programIndex++];
+
+                        if (!un.Exists)
+                        {
+                            un.SetToSingle(goalNum);
+                            break;
+                        }
+                        if (un.IsSingleNumber)
+                        {
+                            if (un.SingleVal % 8 != goalNum)
+                            {
+                                throw new ArgumentOutOfRangeException(nameof(operand), "Invalid Value");
+                            }
+                            break;
+                        }
+                        if (un.IsRange)
+                        {
+                            long result = un.Min + goalNum - (un.Min % 8);
+                            if (result > un.Max)
+                            {
+                                throw new ArgumentOutOfRangeException(nameof(operand), "Invalid Value");
+                            }
+                            un.SetToSingle(result);
+                            break;
+                        }
+                        break;
+                    case 6: //bdv
+                        if (operand is >= 0 and <= 3)
+                        {
+                            divisor = operand;
+                        }
+                        else
+                        {
+                            ref UnknownNumber combo = ref CO(operand, ref A, ref B, ref C);
+                            if (!combo.Exists)
+                            {
+                                combo = new();
+                                combo.SetToSingle(1);
+                            }
+                            if (combo.IsSingleNumber)
+                            {
+                                divisor = combo.SingleVal;
+                            }
+                        }
+                        divisor = (int)Math.Pow(2, divisor);
+
+                        if (A.IsSingleNumber)
+                        {
+                            B.SetToRange(A.SingleVal * divisor, (A.SingleVal * divisor) + divisor - 1);
+                        }
+                        else
+                        {
+                            throw new NotImplementedException();
+                        }
+                        break;
+                    case 7: //cdv
+                        if (operand is >= 0 and <= 3)
+                        {
+                            divisor = operand;
+                        }
+                        else
+                        {
+                            ref UnknownNumber combo = ref CO(operand, ref A, ref B, ref C);
+                            if (!combo.Exists)
+                            {
+                                combo = new();
+                                combo.SetToSingle(1);
+                            }
+                            if (combo.IsSingleNumber)
+                            {
+                                divisor = combo.SingleVal;
+                            }
+                        }
+                        divisor = (int)Math.Pow(2, divisor);
+
+                        A.SetToRange(C.SingleVal * divisor, (C.SingleVal * divisor) + divisor - 1);
+
+                        break;
+                }
+                instructionPointer++;
+                instructionPointer %= instructions.Length;
+            }
+            return A.Min.ToString();
         }
 
-        public int CO(int operand, int A, int B, int C)
+        public long CO(int operand, long A, long B, long C)
         {
             return operand switch
             {
@@ -142,21 +240,93 @@ namespace AdventOfCode2024
             };
         }
 
-        public readonly struct Range
+        public ref UnknownNumber CO(int operand, ref UnknownNumber A, ref UnknownNumber B, ref UnknownNumber C)
         {
-            public int Min { get; }
-            public int Max { get; }
+            switch (operand)
+            {
+                case 4:
+                    return ref A;
+                case 5:
+                    return ref B;
+                case 6:
+                    return ref C;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(operand), "Invalid operand");
+            };
+        }
 
-            public Range(int min, int max)
+        public struct UnknownNumber
+        {
+            public bool Exists { get; set; }
+            public long Min { get; set; }
+            public long Max { get; set; }
+            public bool IsRange { get; set; }
+            public long SingleVal { get; set; }
+            public bool IsSingleNumber { get; set; }
+            //public int Offset { get; set; }
+            //public int Multiplier { get; set; }
+            //public bool IsMultiple { get; set; }
+
+            public UnknownNumber()
+            {
+                Min = -1;
+                Max = -1;
+                IsRange = false;
+                Exists = false;
+                SingleVal = -1;
+                IsSingleNumber = false;
+                //Offset = -1;
+                //Multiplier = -1;
+                //IsMultiple = false;
+            }
+
+            public void SetToRange(long min, long max)
             {
                 Min = min;
                 Max = max;
+                IsRange = true;
+                IsSingleNumber = false;
+                Exists = true;
+                //IsMultiple = false;
             }
 
-            public override string ToString()
+            public void IncreaseMinimum(long min)
             {
-                return $"{Min} to {Max}";
+                Min = min;
+                if (Min == Max)
+                {
+                    SetToSingle(Min);
+                }
             }
+
+            public void DecreaseMaximum(long max)
+            {
+                Max = max;
+                if (Max == Min)
+                {
+                    SetToSingle(Max);
+                }
+            }
+
+            public void SetToSingle(long val)
+            {
+                SingleVal = val;
+                IsRange = false;
+                IsSingleNumber = true;
+                Exists = true;
+                //IsMultiple = false;
+            }
+
+            /*
+            public void SetToMultiple(int offset, int multiplier)
+            {
+                Offset = offset;
+                Multiplier = multiplier;
+                IsRange = false;
+                IsSingleNumber = false;
+                IsMultiple = true;
+            }
+            */
         }
     }
 }
--- obj/Debug/net8.0/AdventOfCode2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdventOfCode2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5127b737be80a907e83a6efb1e81abc19a35a63a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bdaabd5654d5955887c8ce4d100a9d5b08bcb69")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
