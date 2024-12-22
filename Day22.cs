97cbe2fdf056d56081d6601f274efd21ed656e79
Joey Parker <ninjajkl@gmail.com>
2024-12-22 01:02:47 -0500

    feat: added day 22

---
+++ Day22.cs
@@ -0,0 +1,99 @@
+﻿namespace AdventOfCode2024
+{
+    internal class Day22
+    {
+        public string Part1(string inputName)
+        {
+            string[] RawInput = File.ReadAllLines(inputName);
+
+            long sum = 0;
+            foreach (string stringBuyer in RawInput)
+            {
+                long buyer = long.Parse(stringBuyer);
+                sum += CalcSecretNumber(buyer, 2000);
+            }
+            return sum.ToString();
+        }
+
+        public long CalcSecretNumber(long secret, int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                mix(64*secret, ref secret);
+                prune(ref secret);
+                mix(secret/32, ref secret);
+                prune(ref secret);
+                mix(2048*secret, ref secret);
+                prune(ref secret);
+            }
+            return secret;
+        }
+
+        public void mix(long givenValue, ref long secret)
+        {
+            secret ^= givenValue;
+        }
+
+        public void prune(ref long secret)
+        {
+            secret %= 16777216;
+        }
+
+        public string Part2(string inputName)
+        {
+            string[] RawInput = File.ReadAllLines(inputName);
+
+            Dictionary<(int, int, int, int), int[]> monkeyMarket = [];
+            for (int i = 0; i < RawInput.Length; i++)
+            {
+                long buyer = long.Parse(RawInput[i]);
+                CalcSecretMarket(buyer, 2000, monkeyMarket, i, RawInput.Length);
+            }
+
+            return monkeyMarket.Max(kv => kv.Value.Sum()).ToString();
+        }
+
+        public long CalcSecretMarket(long secret, int iterations, Dictionary<(int, int, int, int), int[]> monkeyMarket, int monkeyID, int buyersCount)
+        {
+            int prevOffer;
+            Queue<int> differenceQueue = new();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                prevOffer = (int)(secret % 10);
+
+                mix(64*secret, ref secret);
+                prune(ref secret);
+                mix(secret/32, ref secret);
+                prune(ref secret);
+                mix(2048*secret, ref secret);
+                prune(ref secret);
+
+                int currOffer = (int)(secret % 10);
+                differenceQueue.Enqueue(currOffer-prevOffer);
+                if (differenceQueue.Count > 4)
+                {
+                    differenceQueue.Dequeue();
+                }
+                if (differenceQueue.Count == 4)
+                {
+                    var tuple = differenceQueue.Take(4).ToList();
+                    (int, int, int, int) key = (tuple[0], tuple[1], tuple[2], tuple[3]);
+                    if (monkeyMarket.TryGetValue(key, out int[] monkeyOffers))
+                    {
+                        if (monkeyOffers[monkeyID] != 0)
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        monkeyMarket[key] = new int[buyersCount];
+                    }
+                    monkeyMarket[key][monkeyID] = currOffer;
+                }
+            }
+            return secret;
+        }
+    }
+}
--- obj/Debug/net8.0/AdventOfCode2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdventOfCode2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+066756d74e2594abcfc82948b27d1856e43b620a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a399833090e7aefe33261d7fc35eb7265d3fb61")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
